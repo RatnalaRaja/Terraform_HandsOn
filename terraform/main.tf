c2ed3927228e6b47a0aa6bc17237301793275e2b
Ratnala Raja <rk635238@gmail.com>
2025-08-01 02:37:07 +0530

    main terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,8 +49,14 @@ module "eks" {
 }
 
 # --- OIDC Provider Data Source ---
-data "aws_iam_openid_connect_provider" "eks" {
+data "tls_certificate" "eks" {
   url = module.eks.cluster_oidc_issuer_url
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  url             = module.eks.cluster_oidc_issuer_url
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
 }
 
 # --- IAM Module for IRSA ---
